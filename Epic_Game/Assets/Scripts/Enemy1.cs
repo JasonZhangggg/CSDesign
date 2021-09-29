94aa864afac59eb3da1d73a4d581ce86d8ed9685
lethangomes <35245543+lethangomes@users.noreply.github.com>
2021-09-29 12:23:31 -0700

    More stuff
    Added HUD elements like a reticle and Health bar. Also added a first enemy. The enemy will first look at the player for a bit before charging straight at the player. After a bit of charging the enemy will stop and go back to staring at the player.

---
+++ Epic_Game/Assets/Scripts/Enemy1.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Enemy1 : MonoBehaviour
+{
+    public GameObject player;
+    public bool charging = false;
+    public float windupTime = 5;
+    public float timer = 0;
+    public float speed = 100;
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(!charging)
+        {
+            rb.velocity = Vector3.zero;
+            transform.LookAt(player.transform);
+            timer += Time.deltaTime;
+            if(timer >= windupTime)
+            {
+                timer = 0;
+                charging = true;
+            }
+        }
+        else
+        {
+            rb.AddRelativeForce(Vector3.forward * speed);
+            timer += Time.deltaTime;
+            if(timer >= windupTime/2)
+            {
+                timer = 0;
+                charging = false;
+            }
+        }
+        
+    }
+}
